6878b32152baf0d0260d0e44eaf300157d9e03df
Chessar90 <petereliaschessar@yahoo.com>
2024-12-23 01:39:22 +0300

    Add files via upload
    My project

--- voting.cpp
+++ voting.cpp
@@ -1,4 +1,14 @@
-void castVote(Poll &p, bool vote) {
-    if (vote) p.yesVotes++;
-    else p.noVotes++;
+#include <random> // Include for random number generation
+
+string generateUniqueID() {
+    static int counter = 1;
+    return "Voter" + to_string(counter++);
+}
+
+void registerVoterAuto() {
+    string voterId = generateUniqueID();
+    if (voters.find(voterId) == voters.end()) {
+        voters[voterId] = {voterId, false};
+        cout << "Voter " << voterId << " registered automatically." << endl;
+    }
 }
