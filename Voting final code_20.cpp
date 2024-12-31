114248de4854223e804bc9ed52cc03e1a6a4265f
Chessar90 <petereliaschessar@yahoo.com>
2024-12-31 07:18:20 +0300

    MY project
    The last commit on my voting system project

---
+++ Voting final code_20.cpp
@@ -0,0 +1,6 @@
+// The VotingSystem class manages:
+// - Candidate and voter registration
+// - Voting and result handling
+// - Authentication and data persistence
+
+// Main function ties all logic together using a menu-based interface.
